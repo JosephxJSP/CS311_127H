27f93573176120a514ade100d28117721e1842e7
JosephxJSP <friendliefriend1234@gmail.com>
2024-01-10 12:47:04 +0700

    main

--- config.lua
+++
@@ -1,3 +0,0 @@
-Config = {}
-
-Config["getSharedObject"] = "esx:getSharedObject"
--- workshop1.py
+++
@@ -1,9 +0,0 @@
-print("Hello World")
-print("Nuttanon Rungpiron")
-
-def printme(str, num):
-    print(str, ", lucky number is ", num)
-
-str = "My name is Nuttanon"
-num = 777
-printme(str, num)
